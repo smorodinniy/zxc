2b746f94793a759f17ffd899d72fc0efd54583c2
smorodinniy <nemayatishina@gmail.com>
2024-05-06 10:14:51 +0500

    Update main.go

--- main.go
+++ main.go
@@ -71,3 +71,97 @@ func main() {
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
+
+// Обработчик главной странице.
+func home(w http.ResponseWriter, r *http.Request) {
+	// Проверяется, если текущий путь URL запроса точно совпадает с шаблоном "/". Если нет, вызывается
+	// функция http.NotFound() для возвращения клиенту ошибки 404.
+	// Важно, чтобы мы завершили работу обработчика через return. Если мы забудем про "return", то обработчик
+	// продолжит работу и выведет сообщение "Привет из SnippetBox" как ни в чем не бывало.
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	w.Write([]byte("Привет из Snippetbox"))
+}
+
+// Обработчик для отображения содержимого заметки.
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	// Извлекаем значение параметра id из URL и попытаемся
+	// конвертировать строку в integer используя функцию strconv.Atoi(). Если его нельзя
+	// конвертировать в integer, или значение меньше 1, возвращаем ответ
+	// 404 - страница не найдена!
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	// Используем функцию fmt.Fprintf() для вставки значения из id в строку ответа
+	// и записываем его в http.ResponseWriter.
+	fmt.Fprintf(w, "Отображение выбранной заметки с ID %d...", id)
+}
+
+// Обработчик для создания новой заметки.
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	// Используем r.Method для проверки, использует ли запрос метод POST или нет. Обратите внимание,
+	// что http.MethodPost является строкой и содержит текст "POST".
+	if r.Method != http.MethodPost {
+		// Используем метод Header().Set() для добавления заголовка 'Allow: POST' в
+		// карту HTTP-заголовков. Первый параметр - название заголовка, а
+		// второй параметр - значение заголовка.
+		w.Header().Set("Allow", http.MethodPost)
+
+		// Используем функцию http.Error() для отправки кода состояния 405 с соответствующим сообщением.
+		http.Error(w, "Метод запрещен!", 405)
+
+		// Затем мы завершаем работу функции вызвав "return", чтобы
+		// последующий код не выполнялся.
+		return
+	}
+
+	w.Write([]byte("Создание новой заметки..."))
+}
+
+func main() {
+	// Регистрируем два новых обработчика и соответствующие URL-шаблоны в
+	// маршрутизаторе servemux
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+
+	log.Println("Запуск веб-сервера на http://127.0.0.1:4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	w.Write([]byte("Привет из Snippetbox"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Отображение определенной заметки с ID %d...", id)
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Метод не дозволен", 405)
+		return
+	}
+
+	w.Write([]byte("Создание новой заметки..."))
+}
